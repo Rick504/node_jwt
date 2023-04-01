45d24825d9abe2639e986d81a8209f5b1d48da3b
Rick504 <rick.wes1@gmail.com>
2023-04-01 17:45:24 -0300

    implementando typescript

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Projeto simples em node mostrando a criação de tokens jwt e typescript.
 
 Para acessar a rota /protegido precisa ser efetuado o login na rota /login, assim ira gerar um token.
+
+# Como Rodar:
+
+    yarn dev
+
+# Como buildar:
+
+    yarn build
+
--- src/app.ts
+++ src/app.ts
@@ -11,14 +11,14 @@ app.use(express.json());
 
 //middleware jwt
 function verificaToken(req: Request, res: Response, next: NextFunction) {
-  const token = req.headers['x-access-token'];
+  const token = req.headers['x-access-token'] as string;
 
   if (!token) {
     res.status(401).send('Token não fornecido.');
     return;
   }
 
-  jwt.verify(token, jwtSecret, (err, decoded) => {
+  jwt.verify(token, jwtSecret, (err: any, decoded: any) => {
     if (err) {
       console.log(err);
       res.status(401).send('Token inválido.');
