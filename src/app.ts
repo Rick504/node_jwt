8b95a493d652092ba99809793af5ed2be996741c
Rick504 <rick.wes1@gmail.com>
2023-04-01 19:41:34 -0300

    instalando dependecias que geralmente se usam na maioria das aplicações de grande port

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import appRouter from './routers/index';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(appRouter);
+
+// Cors - settings ---------------------------------------//
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 
 app.listen(process.env.PORT, () => {
   console.log(
